e100508db85037e98ed3dc7249d96034be4950b5
SrAlch94 <daniel.contero@gmail.com>
2022-02-26 23:36:51 +0000

    Movie Database API added

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+// API
+import API from '../API';
 
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
@@ -15,7 +18,32 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
 
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(error);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page)
+
+            setState(prev => ({
+                ...movies,
+                results:
+                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }));
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    };
+
+    // Initial render
+    useEffect(() => {
+        fetchMovies(1)
+    }, []);
+
+    console.log(state);
+
     return <div>Home Page</div>
-}
+};
 
 export default Home;

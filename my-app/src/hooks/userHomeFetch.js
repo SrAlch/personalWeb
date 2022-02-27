bd9e503082d930497dfb5cfce3d37f3d42429e92
SrAlch94 <daniel.contero@gmail.com>
2022-02-27 18:45:20 +0000

    Loading more button added

--- my-app/src/hooks/userHomeFetch.js
+++ my-app/src/hooks/userHomeFetch.js
@@ -15,6 +15,7 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
 
     const fetchMovies = async (page, searchTerm = "") => {
         try {
@@ -34,10 +35,11 @@ export const useHomeFetch = () => {
         setLoading(false);
     };
 
-    // Initial render
+    // Initial and search
     useEffect(() => {
-        fetchMovies(1)
-    }, []);
+        setState(initialState);
+        fetchMovies(1, searchTerm)
+    }, [searchTerm]);
 
-    return { state, loading, error, setSearchTerm }
+    return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore }
 };
